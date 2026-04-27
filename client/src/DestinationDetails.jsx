d129f8026194c909bfb4b8810f8c27347aa1eaa1
Hamna-Sherin <hamnaadiyottil@gmail.com>
2026-04-27 12:32:16 +0530

    responsive

--- client/src/DestinationDetails.jsx
+++ client/src/DestinationDetails.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
 const DestinationDetails = () => {
