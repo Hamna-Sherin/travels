b9f4687e0f7a03c835c10af92ce02a03a07153cb
Hamna-Sherin <hamnaadiyottil@gmail.com>
2026-05-11 09:28:23 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,11 +23,23 @@ import Packages from './admin/Packages';
 import AdminTaxiBookings from './admin/Bookings';
 import Users from './admin/Users';
 import AddDestination from './admin/AddDestination';
-import EditDestination from '../../admin/src/EditDestination';
+import EditDestination from './admin/EditDestination';
 import AddPackage from './admin/AddPackages';
 import EditPackage from './admin/EditPckages';
 import AdminEnquiries from './admin/Enquiry';
 import AdminPackageBookings from './admin/PackageBookings';
+// import AdminLayout from './admin/AdminLayout';
+// import AdminDashboard from './admin/Dashboard';
+// import Destinations from './admin/Destination';
+// import Packages from './admin/Packages';
+// import AdminTaxiBookings from './admin/Bookings';
+// import Users from './admin/Users';
+// import AddDestination from './admin/AddDestination';
+// import EditDestination from '../../admin/src/EditDestination';
+// import AddPackage from './admin/AddPackages';
+// import EditPackage from './admin/EditPckages';
+// import AdminEnquiries from './admin/Enquiry';
+// import AdminPackageBookings from './admin/PackageBookings';
 
 
 const App = () => {
